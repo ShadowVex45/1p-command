44f09204be39d0d3cfa80d3165cabf6841a696a8
ShadowVex45 <muhammadayanshahzad1@gmail.com>
2026-03-05 12:53:00 +0300

    Activate '1p' command in current session after install
    Source .bashrc to activate '1p' command immediately after installation.

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@ ALIAS_LINE="alias 1p=\"hostname -I | awk '{print \$1}'\""
 if ! grep -q "alias 1p=" ~/.bashrc; then
     echo "$ALIAS_LINE" >> ~/.bashrc
     echo "Success! '1p' command installed."
-    echo "Please run 'source ~/.bashrc' or restart your terminal to use it."
+    # This line below will now execute within the current session 
+    # IF the user runs the script using 'source'
+    source ~/.bashrc
+    echo "The '1p' command is now active and ready to use!"
 else
     echo "'1p' command is already installed."
 fi

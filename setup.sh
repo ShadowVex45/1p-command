32a777b47c51944319ee396716786e27f84e6db5
ShadowVex45 <muhammadayanshahzad1@gmail.com>
2026-03-05 12:53:45 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,6 @@ ALIAS_LINE="alias 1p=\"hostname -I | awk '{print \$1}'\""
 if ! grep -q "alias 1p=" ~/.bashrc; then
     echo "$ALIAS_LINE" >> ~/.bashrc
     echo "Success! '1p' command installed."
-    # This line below will now execute within the current session 
-    # IF the user runs the script using 'source'
     source ~/.bashrc
     echo "The '1p' command is now active and ready to use!"
 else

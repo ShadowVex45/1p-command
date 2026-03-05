7024a4ee761b385e1de46ae54817a84d10342ec8
ShadowVex45 <muhammadayanshahzad1@gmail.com>
2026-03-05 12:59:31 +0300

    Update uninstall_1p.sh

--- uninstall_1p.sh
+++ uninstall_1p.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# 1. Remove from the file (Permanent)
+# 1. Remove from the file so it doesn't come back on reboot
 if [ -f ~/.bashrc ]; then
     sed -i '/alias 1p=/d' ~/.bashrc
 fi
 
-# 2. Kill it from memory (Instant)
+if [ -f ~/.zshrc ]; then
+    sed -i '/alias 1p=/d' ~/.zshrc
+fi
+
+# 2. Kill it from the active memory
+# Note: This only works if the user runs the script with 'source'
 unalias 1p 2>/dev/null
 
-echo "Removed '1p' from .bashrc."
-echo "Success! '1p' command has been uninstalled."
+echo "Removed '1p' from config files."
+echo "Success! If '1p' still works, type: unalias 1p"
